a56420d71ce95d531976fe6095369daf74b1a56e
Logon Aluno <logonrmlocal@fiap.com.br>
2024-03-20 20:53:09 -0300

    Segunda aula

--- app/src/main/java/fiap/com/br/orgs/MainActivity.kt
+++ app/src/main/java/fiap/com/br/orgs/MainActivity.kt
@@ -8,5 +8,19 @@ class MainActivity : Activity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Binding: processo de vinculação do
+        // código fonte com o arquivo de layout.
+
+        val nome = findViewById<TextView>(R.id.nome)
+        //nome.setText("Cesta de frutas")
+        nome.text = "Cesta de frutas"
+
+        val descricao = findViewById<TextView>(R.id.descricao)
+        descricao.text = "Laranja, manga e uva"
+
+        val valor = findViewById<TextView>(R.id.valor)
+        valor.text = "R$19.99"
+
     }
 }
